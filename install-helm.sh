f62d3c2e898438c195c887fce4646fe28573528b
ChichiCaleb <ChichiCaleb@chichiuchenna@gmail.com>
2024-08-04 04:15:16 -0700

    update

--- install-helm.sh
+++ install-helm.sh
@@ -8,7 +8,8 @@ fi
 # Substitute environment variables in the template file
 envsubst < values-template.yaml > values.yaml
 
-kubectl create ns alustan
+kind create cluster 
 
+kubectl create ns alustan
 
 helm install alustan-controller oci://registry-1.docker.io/alustan/alustan-helm --version <version> --timeout 20m0s --debug --atomic --values values.yaml 
