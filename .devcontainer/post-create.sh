9aff3b9cfab039f0fe06044a0116cf734f6eefe2
ChichiCaleb <ChichiCaleb@chichiuchenna@gmail.com>
2024-08-04 01:50:15 -0700

    updaate dev container

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,11 +3,10 @@
 # this runs at Codespace creation - not part of pre-build
 
 echo "post-create start"
-echo "$(date)    post-create start" >> "$HOME/status"
+echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create start" >> "$HOME/status"
 
-# update the repos
-git -C /workspaces/imdb-app pull
-git -C /workspaces/webvalidate pull
+# Create k3d cluster with a specific Kubernetes version
+k3d cluster create mycluster --image rancher/k3s:v1.23.0-k3s1 --api-port 6443 -p "30000-32767:30000-32767@server[0]" --agents 2
 
 echo "post-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -8,7 +8,6 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
 # update the base docker images
 docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
 docker pull mcr.microsoft.com/dotnet/sdk:6.0
-docker pull ghcr.io/cse-labs/webv-red:latest
 
 echo "post-start complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start complete" >> "$HOME/status"
