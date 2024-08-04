f78e6daa0f8f6ff6818ebaf15290dfa906186734
ChichiCaleb <ChichiCaleb@chichiuchenna@gmail.com>
2024-08-04 02:11:59 -0700

    update k3d image

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,7 +6,7 @@ echo "post-create start"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create start" >> "$HOME/status"
 
 # Create k3d cluster with a specific Kubernetes version
-k3d cluster create mycluster --image rancher/k3s:v1.23.0-k3s1 --api-port 6443 -p "30000-32767:30000-32767@server[0]" --agents 2
+k3d cluster create mycluster --image rancher/k3s:v1.23.8-k3s1
 
 echo "post-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
